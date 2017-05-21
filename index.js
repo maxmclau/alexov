cda30599aad99d41709cf598d4f748f612a374d2
Maxmillion McLaughlin <github@maxmclau.com>
2017-05-20 23:50:43 -0400

    Rough lyrics retrieval and spotofy integration

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * The contents of this file is free and unencumbered software released into the
- * public domain. For more information, please refer to <http://unlicense.org/>
- *
- * @author Maxmillion McLaughlin <npm@maxmclau.com>
- */
-
-'use strict';
